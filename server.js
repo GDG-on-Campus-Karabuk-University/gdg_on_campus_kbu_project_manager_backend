31a58a0d03ef37aa4b1cc34f5bcaaee5ab7bffcc
Yusuf GOKKAYA <yusuf.gokkaya0@outlook.com>
2024-10-23 21:02:47 +0300

    dotenv kuruldu ve cfg dosyası oluşturuldu

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+// Enviroment Variables
+
+
+dotenv.config({
+    path : "./config/env/config.env"
+});
+
 
 const app = express();
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT;
 
 
 app.get("/", (req, res) => {
     res.send("Hello GDG Backend API - UPDATED");
 })
 app.listen(PORT, () => {
-    console.log(`App Started on ${PORT}`);
+    console.log(`App Started on ${PORT} : ${process.env.NODE_ENV}`);
 });
