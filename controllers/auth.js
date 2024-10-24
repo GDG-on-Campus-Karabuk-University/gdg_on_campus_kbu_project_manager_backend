22dcd58f91ed4e89f56ad243e98a9bc65e97beb0
Yusuf GOKKAYA <yusuf.gokkaya0@outlook.com>
2024-10-24 19:45:03 +0300

    Error handling mekanizmaları eklendi - expressasynchandler kuruldu

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,33 @@
-const register = (req, res) => {
+const User = require("../models/User");
+const CustomError = require("../helpers/error/CustomError");
+const asyncErrorWrapper = require("express-async-handler");
+
+const register = asyncErrorWrapper(async (req, res, next) => {
+
+    const name = "Emirhan Soylu";
+    const email = "emosan12@gmail.com";
+    const password = "123456";
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
     res.status(200)
     .json({
-        success: true,
+            user
+    });
 
-    })
-};
+});
+
+const errorTest = (req, res, next) => {
+
+    return next(new TypeError("Type Error"));
+
+}
 
 module.exports = {
-    register
+    register,
+    errorTest
 }
