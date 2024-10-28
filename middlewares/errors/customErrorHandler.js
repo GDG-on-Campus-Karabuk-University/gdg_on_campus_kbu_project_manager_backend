866e72753578132b25a8e4d3e98e4fb966eabda1
Yusuf GOKKAYA <yusuf.gokkaya0@outlook.com>
2024-10-28 15:45:31 +0300

    getusers tamamlandı

--- middlewares/errors/customErrorHandler.js
+++ middlewares/errors/customErrorHandler.js
@@ -2,7 +2,7 @@ const CustomError = require("../../helpers/error/CustomError");
 const customErrorHandler = (err, req, res, next) => {
 
     let customError = err;
-    console.log(err, err.name, err.message);
+    console.log(err.name, err.message);
 
     if (err.name === "SyntaxError") {
         customError = new CustomError("Unexpected Syntax", 400);
@@ -12,6 +12,9 @@ const customErrorHandler = (err, req, res, next) => {
     }
     if (err.name === "MongoServerError") {
         customError = new CustomError("Duplicate Key Found: Check Your Input", 400);
+    }
+    if (err.name === "CastError") {
+        customError = new CustomError("Please provide a valid id", 400)
     }
 
     res.status(customError.status || 500)
