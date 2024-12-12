289b5c8346a114d744e8766f78d2b69210fe1e49
Yusuf GOKKAYA <yusuf.gokkaya0@outlook.com>
2024-12-12 17:53:12 +0300

    chat için socket.io halledildi, register düzeltildi

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const dotenv = require("dotenv");
 const connectDatabase = require("./helpers/database/connectDatabase.js");
 const customErrorHandler = require("./middlewares/errors/customErrorHandler.js");
 const path = require("path");
+const { createServer } = require("http");
+const socketSetup = require("./socket.js");
+const cors = require("cors");
+const helmet = require("helmet");
 
-
-const routers = require("./routers/index.js")
+const routers = require("./routers/index.js");
 
 // Enviroment Variables
 
@@ -19,12 +22,38 @@ dotenv.config({
 connectDatabase();
 
 const app = express();
+const httpServer = createServer(app);
+
+app.use(cors());
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://cdn.socket.io"],
+            connectSrc: ["'self'", "ws://localhost:5000", "http://localhost:5000"],
+            styleSrc: ["'self'", "'unsafe-inline'"],
+            imgSrc: ["'self'", "data:", "blob:"],
+            fontSrc: ["'self'", "https:", "data:"],
+        },
+    },
+}));
+
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+// Socket.IO'nun yapılandırmasını burada başlatıyoruz
+
+socketSetup(io);
 
 // Express - Body Middleware;
 
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // Routers Middleware
 
@@ -39,6 +68,6 @@ app.use(customErrorHandler);
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`App Started on ${PORT} : ${process.env.NODE_ENV}`);
 });
